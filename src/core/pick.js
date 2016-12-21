b6ab94971ef43daf0bee7625c99629fb8006d660
Calvin Chan <calvin@spright.com>
2016-12-21 15:09:02 -0800

    Safeguard empty data.

--- src/core/pick.js
+++ src/core/pick.js
@@ -2,6 +2,10 @@ import basePickBy from './base/basePickBy'
 import hasKeyIn from './util/hasKeyIn'
 
 export default function pick(data, maybePath) {
+  if (data == null) {
+    return {}
+  }
+
   return basePickBy(data, maybePath, function (value, path) {
     return hasKeyIn(data, path)
   })
--- src/core/pickBy.js
+++ src/core/pickBy.js
@@ -3,18 +3,13 @@ import getIteratee from './util/getIteratee'
 import basePickBy from './base/basePickBy'
 
 export default function pickBy(data, predicate) {
-  // TODO check if data is null
+  if (data == null) {
+    return {}
+  }
+
   const props = getAllKeys(data)
   predicate = getIteratee(predicate)
   return basePickBy(data, props, function(value, path) {
     return predicate(value, path[0])
   })
 }
-
-// var props = arrayMap(getAllKeysIn(object), function(prop) {
-//   return [prop];
-// });
-// predicate = getIteratee(predicate);
-// return basePickBy(object, props, function(value, path) {
-//   return predicate(value, path[0]);
-// });
